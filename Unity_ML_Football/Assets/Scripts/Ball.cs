58474ce8b5c4bd19e61abed043a62ed06a0d8628
BlackBloodE <elton9731@gmail.com>
2020-04-10 05:56:57 +0800

    踢足球 設定機器學習控制環境、事件開始與成功失敗內容 第三章

---
+++ Unity_ML_Football/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    /// <summary>
+    /// 足球是否進入球門
+    /// </summary>
+
+    public static bool complete;
+
+    /// <summary>
+    /// 觸發事件開始:碰到勾選Is Trigger 物件會執行一次
+    /// </summary>
+    /// <param name="other">碰到的物件資訊</param>
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // 如果 碰到物件 的名稱 等於 "進球感應區"
+        if (other.name == "進球感應區")
+        {
+            //進入球門
+            complete = true;
+        }
+    }
+}
